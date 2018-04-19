38b95bc47d3706542497dfea530e7957398c1e90
Dani Vijay <danimvijay@gmail.com>
2018-04-19 21:56:36 +0530

    init commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-toolbar
+      app
+    >
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-footer app>
+      <span>&copy; 2018</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Finder App'
+    }
+  },
+  name: 'App'
+}
+</script>
