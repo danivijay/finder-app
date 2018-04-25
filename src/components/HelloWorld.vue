92ead3b4218d88fb99414b41867b80401021e07a
Dani Vijay <danimvijay@gmail.com>
2018-04-25 21:07:26 +0530

    home page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,67 +1,36 @@
 <template>
-  <v-container fluid>
-    <v-layout  row wrap>
-      <v-flex xs12 class="mb-3">
-        <v-card>
-          <v-card-text>
+  <v-container>
+    <v-layout  row wrap class="mb-3">
+      <v-flex xs9>
             <v-text-field
               label="Search person"
             ></v-text-field>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
+      </v-flex>
+      <v-flex xs3>
             <v-btn
+            block
+            large
+            color="primary"
             >Search</v-btn>
-          </v-card-actions>
-        </v-card>
       </v-flex>
     </v-layout>
     <v-layout row wrap>
-      <v-flex xs6 sm4 md3 lg2 class="pl-2">
+      <v-flex xs12 sm6 md4 class="pl-2">
         <v-card>
-          <v-card-media src="https://lh4.googleusercontent.com/-M6gI9NNnr0k/AAAAAAAAAAI/AAAAAAAAAB8/8quus1Eouio/photo.jpg" height="200px">
+          <v-card-media src="https://lh4.googleusercontent.com/-M6gI9NNnr0k/AAAAAAAAAAI/AAAAAAAAAB8/8quus1Eouio/photo.jpg" height="400px">
           </v-card-media>
           <v-card-title primary-title>
             <div>
-              <h3 class="headline mb-0">Ramesh</h3>
-              <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
+              <h3 class="headline mb-0">John Doe</h3>
+              <h4>ID: 1247854</h4>
+              <div>Missing from Vytila, Kochi from 20-10-2017</div>
             </div>
           </v-card-title>
           <v-card-actions>
-            <v-btn flat color="orange">Share</v-btn>
-            <v-btn flat color="orange">Details</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-      <v-flex xs6 sm4 md3 lg2 class="pl-2">
-        <v-card>
-          <v-card-media src="https://lh4.googleusercontent.com/-M6gI9NNnr0k/AAAAAAAAAAI/AAAAAAAAAB8/8quus1Eouio/photo.jpg" height="200px">
-          </v-card-media>
-          <v-card-title primary-title>
-            <div>
-              <h3 class="headline mb-0">Suresh</h3>
-              <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
-            </div>
-          </v-card-title>
-          <v-card-actions>
-            <v-btn flat color="orange">Share</v-btn>
-            <v-btn flat color="orange">Details</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-      <v-flex xs6 sm4 md3 lg2 class="pl-2">
-        <v-card>
-          <v-card-media src="https://lh4.googleusercontent.com/-M6gI9NNnr0k/AAAAAAAAAAI/AAAAAAAAAB8/8quus1Eouio/photo.jpg" height="200px">
-          </v-card-media>
-          <v-card-title primary-title>
-            <div>
-              <h3 class="headline mb-0">Sateesh</h3>
-              <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
-            </div>
-          </v-card-title>
-          <v-card-actions>
-            <v-btn flat color="orange">Share</v-btn>
-            <v-btn flat color="orange">Details</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn dark color="success">Share</v-btn>
+            <v-btn dark color="error">Report</v-btn>
+            <v-btn dark color="info">Details</v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
